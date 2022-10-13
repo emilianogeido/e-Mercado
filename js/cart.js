65f68fcf180f958bd97af1aa4f79e29f52173cca
emilianogeido <110432365+emilianogeido@users.noreply.github.com>
2022-10-13 16:27:19 -0300

    Carrito semi completado

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,7 @@
 const usuario = 25801
 
 function getHTML(article) {
-   return ` 
+  return ` 
    <tr>
    <th scope="row"> <img src="${article.image}" class="img-fluid" alt="" height="70" width="70"></th>
    <td>${article.name}</td>
@@ -12,30 +12,35 @@ function getHTML(article) {
        <input type="number" class= "form-control" id="input-cantidad"  >
    </div>
  </td>
-   <td>${article.unitCost}</td>
+   <td id="subtotal"></td>
  </tr>
  `;
 
 }
 
+
+
 document.addEventListener("DOMContentLoaded", async function () {
-    const listado = document.querySelector('#tBody-Carrito');
-    const Json = await getJSONData(CART_INFO_URL + usuario + EXT_TYPE);
-    console.log(Json)
-    let array = Json.data.articles
-    console.log(array)
-    array.forEach(array => {
-          listado.innerHTML += getHTML(array);
-    })
+  const listado = document.querySelector('#tBody-Carrito');
+  const Json = await getJSONData(CART_INFO_URL + usuario + EXT_TYPE);
+  console.log(Json)
+  var array = Json.data.articles
+  console.log(array)
+  array.forEach(array => {
+    listado.innerHTML += getHTML(array);
 
-    var input = document.getElementById("input-cantidad");
+    document.addEventListener("change", () => {
 
-    document.addEventListener(click , () => {
-    
       var input = document.getElementById("input-cantidad");
       var valor = input.value;
-    
-      alert("El valor del campo es:"+ valor);
-      
+
+      if (valor >= 0) {
+        document.getElementById("subtotal").innerHTML = 'USD'+ valor * array.unitCost
+      }
+
     });
- });
+
+  })
+
+
+});
