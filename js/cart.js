7fa07895db4678cac39ce451a06a048ea1acc057
emilianogeido <110432365+emilianogeido@users.noreply.github.com>
2022-10-25 19:52:22 -0300

    Mejoras en Cart
    -Subtotal general
    -Costo de envió
    -total a pagar

--- js/cart.js
+++ js/cart.js
@@ -24,24 +24,55 @@ document.addEventListener("DOMContentLoaded", async function () {
   const listado = document.querySelector('#tBody-Carrito');
   const Json = await getJSONData(CART_INFO_URL + usuario + EXT_TYPE);
   var array = Json.data.articles
+  
   array.forEach(array => {
     listado.innerHTML += getHTML(array);
+    
+    /*---  poner subtotal general   --- */
+    const subtotalGeneral = document.querySelector('#subtotalGeneral');
+    subtotalGeneral.innerHTML = array.currency + array.unitCost
 
+    /*---  Ver que radio seleciona   --- */
+    const radioButtons = document.querySelectorAll('input[name="flexRadioDefault"]');
+    var valorPorcentaje;
+    document.addEventListener("change", () => {
+        for (const radioButton of radioButtons) {
+            if (radioButton.checked) {
+              valorPorcentaje = parseFloat(radioButton.value) ;
+               console.log(valorPorcentaje)
+                break;
+            }
+        }
+      });
+
+      
+      
+
+      /*--- actualizando valores   --- */
     document.addEventListener("change", () => {
 
       var input = document.getElementById("input-cantidad");
       var valor = input.value;
-      
-
+    
       if (valor >= 0) {
-        document.getElementById("subtotal").innerHTML = 'USD'+ valor * array.unitCost
+        document.getElementById("subtotal").innerHTML = array.currency + valor * array.unitCost
+        
+        subtotalGeneral.innerHTML = array.currency + valor * array.unitCost
+        const costoEnvio = document.querySelector('#costoEnvio');
+        costoEnvio.innerHTML = array.currency + (( valor * array.unitCost) * valorPorcentaje)
+        const totalPagar = document.querySelector('#totalPagar');
+        totalPagar.innerHTML = array.currency + (( valor * array.unitCost) * valorPorcentaje + valor * array.unitCost )
+        
       }
+
 
     });
 
-    
+  
 
   })
+
+  
 
 
 });
