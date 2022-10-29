91d3f4d549b79ece6ae57eaa9e81989d1b7b6196
emilianogeido <110432365+emilianogeido@users.noreply.github.com>
2022-10-29 19:32:15 -0300

    cart updates
    -validation

--- js/cart.js
+++ js/cart.js
@@ -1,28 +1,87 @@
 
 const usuario = 25801
-const forms = document.querySelectorAll("form");
+const forms = document.querySelectorAll('form');
 const tarjeta = document.querySelector("#tarjeta");
 const transferencia = document.querySelector("#transferenciaBancaria");
 const numeroCuenta = document.querySelector("#numeroCuenta");
 const numeroTarjeta = document.querySelector("#numeroTarjeta");
 const codigoSeg = document.querySelector("#codigoSeg");
 const vencimiento = document.querySelector("#vencimiento");
+const btnComprar = document.querySelector("#btn-comprar");
+const btnLinkTerms = document.querySelector("#btn-link-terms")
+const divMustAcceptTerms = document.querySelector("#div-must-accept-terms")
 
+
+
+const verifyRadioState = () => {
+  if (!tarjeta.checked && !transferencia.checked) {
+    btnLinkTerms.classList.add("link-danger");
+    divMustAcceptTerms.classList.add("d-inline");
+  } else if (!numeroCuenta.checkValidity()) {
+    btnLinkTerms.classList.add("link-danger");
+    divMustAcceptTerms.classList.add("d-inline");
+  } else if (!numeroTarjeta.checkValidity()) {
+    btnLinkTerms.classList.add("link-danger");
+    divMustAcceptTerms.classList.add("d-inline");
+  } else if (!codigoSeg.checkValidity()) {
+    btnLinkTerms.classList.add("link-danger");
+    divMustAcceptTerms.classList.add("d-inline");
+  } else if (!vencimiento.checkValidity()) {
+    btnLinkTerms.classList.add("link-danger");
+    divMustAcceptTerms.classList.add("d-inline");
+  }
+  else {
+    btnLinkTerms.classList.remove("link-danger");
+    divMustAcceptTerms.classList.remove("d-inline");
+  }
+};
+
+
+/* summit all forms */
+frmsSbmt = (forms) => {
+  console.log(forms);
+  forms.forEach((form) => {
+
+    form.onchange = (frmSbmt) => {
+      if (!form.checkValidity()) {
+        frmSbmt.preventDefault();
+        frmSbmt.stopPropagation();
+      }
+    };
+    form.classList.add("was-validated");
+  });
+};
+
+
+
+btnComprar.onclick = () => {
+  frmsSbmt(forms);
+  verifyRadioState();
+  console.log('sumit')
+};
+
+let form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Compra exitosa");
+});
 
 tarjeta.onclick = () => {
-  transferencia.checked=false;
+  transferencia.checked = false;
   numeroCuenta.disabled = true
   vencimiento.disabled = false
   codigoSeg.disabled = false
   numeroTarjeta.disabled = false
+  transferencia.required = false
 };
 
 transferencia.onclick = () => {
-  tarjeta.checked=false;
+  tarjeta.checked = false;
   vencimiento.disabled = true
   codigoSeg.disabled = true
   numeroTarjeta.disabled = true
   numeroCuenta.disabled = false
+  tarjeta.required = false
 };
 
 
@@ -35,7 +94,7 @@ function getHTML(article) {
    <td>${article.currency}${article.unitCost}</td>
    <td>  
      <div class="col-2">
-       <input type="number" value="1" class= "form-control" id="input-cantidad"  >
+       <input type="number" value="1" class= "form-control" id="input-cantidad" min="1" required >
    </div>
  </td>
    <td id="subtotal">${article.currency}${article.unitCost}</td>
@@ -50,10 +109,10 @@ document.addEventListener("DOMContentLoaded", async function () {
   const listado = document.querySelector('#tBody-Carrito');
   const Json = await getJSONData(CART_INFO_URL + usuario + EXT_TYPE);
   var array = Json.data.articles
-  
+
   array.forEach(array => {
     listado.innerHTML += getHTML(array);
-    
+
     /*---  poner subtotal general   --- */
     const subtotalGeneral = document.querySelector('#subtotalGeneral');
     subtotalGeneral.innerHTML = array.currency + array.unitCost
@@ -62,43 +121,48 @@ document.addEventListener("DOMContentLoaded", async function () {
     const radioButtons = document.querySelectorAll('input[name="flexRadioDefault"]');
     var valorPorcentaje;
     document.addEventListener("change", () => {
-        for (const radioButton of radioButtons) {
-            if (radioButton.checked) {
-              valorPorcentaje = parseFloat(radioButton.value) ;
-               console.log(valorPorcentaje)
-                break;
-            }
+      for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+          valorPorcentaje = parseFloat(radioButton.value);
+          console.log(valorPorcentaje)
+          break;
         }
-      });
+      }
+    });
 
-      
-      
 
-      /*--- actualizando valores   --- */
+
+
+    /*--- actualizando valores   --- */
     document.addEventListener("change", () => {
 
       var input = document.getElementById("input-cantidad");
       var valor = input.value;
-    
+      if (input <= 0) {
+        input.classList.add("link-danger");
+      }
+
+
       if (valor >= 0) {
         document.getElementById("subtotal").innerHTML = array.currency + valor * array.unitCost
-        
+
         subtotalGeneral.innerHTML = array.currency + valor * array.unitCost
         const costoEnvio = document.querySelector('#costoEnvio');
-        costoEnvio.innerHTML = array.currency + (( valor * array.unitCost) * valorPorcentaje)
+        costoEnvio.innerHTML = array.currency + ((valor * array.unitCost) * valorPorcentaje)
         const totalPagar = document.querySelector('#totalPagar');
-        totalPagar.innerHTML = array.currency + (( valor * array.unitCost) * valorPorcentaje + valor * array.unitCost )
-        
+        totalPagar.innerHTML = array.currency + ((valor * array.unitCost) * valorPorcentaje + valor * array.unitCost)
+
       }
 
 
     });
 
-  
+
 
   })
 
-  
+
 
 
 });
+
